673762a54892b2e088bfecfc3813375c24fda15b
AlirezaOrouji <mail.alirezaorouji@gmail.com>
2022-06-04 12:58:51 +0430

    Add Redis settings

--- KaveNegar.Project/Program.cs
+++ KaveNegar.Project/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KaveNegar.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace KaveNegar.Project
     {
         static void Main(string[] args)
         {
+            Console.ReadKey();
         }
     }
 }
